a1c596c4fc08371a4c23a4167ae745b68ad52e5f
trevillie <zbshfmmm@163.com>
2015-04-28 15:08:27 +0800

    basic utilities and multithreading for Meow

--- src/controller/Meow.py
+++ src/controller/Meow.py
@@ -1,6 +1,8 @@
 import os
 import re
+from time import sleep
 import subprocess
+import threading
 
 
 """
@@ -24,6 +26,13 @@ class Meow:
 
   def __init__(self):
     self.log_clear_sig = "Trevillie"
+    self.started_sig = "apeDroid"
+    self.trigger_sig = ""
+
+    self.on = False
+    
+    self.trigger_time = ""
+    self.started_time = ""
     self.log_clear_note = {
       "V" : True,
       "D" : True,
@@ -34,13 +43,33 @@ class Meow:
     print "Logcat Meowing..."
 
 
+  def turn_on(self, main_activity):
+    """
+    Pass main_activity as a string.
+    """
+    self.trigger_sig = main_activity
+    self.on = True
+    print "Kitty turned on with", main_activity
+
+
+  def turn_off(self):
+    self.on = False
+    print "Kitty turned off with", self.trigger_sig
+    self.take_note()
+    self.trigger_sig = ""
+
+
   def refresh(self):
     self.__init__()
   
 
   def take_note(self):
     # clear & time
-    pass
+    print "------------------------------------"
+    print "trigger time :", self.trigger_time
+    print "started time :", self.started_time
+    print "logcat clear record :", self.log_clear_note
+    print "------------------------------------"
 
 
   def clear(self):
@@ -56,6 +85,11 @@ class Meow:
       return False
 
 
+  def thread_up(self):
+    t = threading.Thread(target=self.run)
+    t.start()
+
+
   def run(self):
     input = os.popen("adb logcat -v time")
     while True:
@@ -69,25 +103,31 @@ class Meow:
       if not match is None:
         date, time, tagtype, tag, owner, message = match.groups()
         
-        # check if logs are cleaned
-        if tag == self.log_clear_sig:
-          self.log_clear_note[tagtype] = False
-          print self.log_clear_note
-        
-        """
-        m = regex["died"].match(message)
-        if not m is None:
-          proc_name, pid = m.groups()
-          if procs.has_key(pid):
-            procs.pop(pid)
-          else:
-            print "---", pid, "started not captured"
-          print proc_name, pid, "died"
-        """
+        if self.on:
+          # check if logs are cleaned
+          if self.log_clear_sig in tag:
+            self.log_clear_note[tagtype] = False
 
-      if len(line) == 0: break
+          # take down first trigger time
+          if self.trigger_sig != "":
+            if "START u" in message and self.trigger_sig in message:
+              if self.trigger_time == "":
+                self.trigger_time = time
+
+          # take down first started time
+          if self.started_sig in tag:
+            if self.started_time == "":
+              self.started_time = time
+
+      if len(line) == 0:
+        print "Logcat Terminates..."
+        break
 
 
 if __name__ == "__main__":
   kitty = Meow()
-  kitty.run()
+  kitty.thread_up()
+  sleep(3)
+  kitty.turn_on("news")
+  sleep(10)
+  kitty.turn_off()
